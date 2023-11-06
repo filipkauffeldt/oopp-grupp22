0e5bc325ab7a8592a8b64e4abbe04865cbf24100
Oskmey <qcube.gaming@gmail.com>
2023-11-06 17:38:32 +0100

    This adds elastic collision (not tested)
    I followed a paper that describes solving 2-dimensional elastic collision problems for circles without complicated trigonometry. Might only work with circles.
    
    Link:https://www.vobarian.com/collisions/2dcollisions2.pdf

--- crocket/src/main/java/com/crocket/App.java
+++ crocket/src/main/java/com/crocket/App.java
@@ -1,5 +1,5 @@
 package com.crocket;
-
+import java.lang.Math;
 /**
  * Hello world!
  *
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        double a = Math.atan2(1, 0);
+        System.out.print(a);
     }
 }
