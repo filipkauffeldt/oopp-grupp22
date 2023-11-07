0187a8e6df0e0f9caede28c8f454eae09f0ad69b
Oskar Meyer <112823957+Oskmey@users.noreply.github.com>
2023-11-07 13:15:55 +0100

    Update AppTest.java removed test

--- crocket/src/test/java/com/crocket/AppTest.java
+++ crocket/src/test/java/com/crocket/AppTest.java
@@ -9,12 +9,5 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+
 }
