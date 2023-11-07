68c479b46db728e7513e4203e85a838b11a11dd1
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2023-11-07 17:04:19 +0100

    Redid part of the constructor

--- crocket/src/main/java/com/crocket/Ball.java
+++ crocket/src/main/java/com/crocket/Ball.java
@@ -3,12 +3,15 @@ public class Ball extends Entity implements IMovable{
     private double power;
     private double mass;
     private String powerUp;
+    private double xVelocity;
+    private double yVelocity;
 
-    public Ball(int width, int height, Color color, Texture texture) {
-        super(width, height, color, texture);
+    public Ball(int width, int height, double xPosition, double yPosition) {
+        super(width, height, xPosition, yPosition);
         //TODO Auto-generated constructor stub
     }
 
     public void move(){
+        
     }
 }
--- crocket/src/main/java/com/crocket/Obstacle.java
+++ crocket/src/main/java/com/crocket/Obstacle.java
@@ -1,13 +1,12 @@
 package crocket.src.main.java.com.crocket;
 import crocket.src.main.java.com.crocket.Entity;
 
-import crocket.src.main.java.com.crocket.Color;
 
 public abstract class Obstacle extends Entity {
 
 
-    public Obstacle(int width, int height, Color color, Texture texture) {
-        super(width, height, color, texture);
+    public Obstacle(int width, int height, double xPosition, double yPosition) {
+        super(width, height, xPosition, yPosition);
         //TODO Auto-generated constructor stub
     }
     
