d6e7c7537980963bf73c9a25a88f0e86e2b23800
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2023-11-07 17:01:09 +0100

    renamed BowGoal to Hoop, changed some features

---
+++ crocket/src/main/java/com/crocket/Hoop.java
@@ -0,0 +1,12 @@
+package crocket.src.main.java.com.crocket;
+public class Hoop extends Entity {
+
+
+
+    public Hoop(int width, int height, double xPosition, double yPosition, Direction dir) {
+        super(width, height, xPosition, yPosition);
+        //TODO Auto-generated constructor stub
+    }
+
+
+}
