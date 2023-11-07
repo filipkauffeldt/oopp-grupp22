af9d3abb510454893713a9dc31b92735fa353566
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2023-11-07 17:00:39 +0100

    renamed StickGoal to Peg, changed features

---
+++ crocket/src/main/java/com/crocket/Peg.java
@@ -0,0 +1,11 @@
+package crocket.src.main.java.com.crocket;
+import crocket.src.main.java.com.crocket.Entity;
+
+public class Peg extends Entity {
+
+    public Peg(int width, int height, double xPosition, double yPosition) {
+        super(width, height, xPosition, yPosition);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
