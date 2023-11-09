b9052b2a15a0aa70abeab4242fd115c8ee96dbdd
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2023-11-09 15:22:20 +0100

    added a reset method for hoop and peg

--- crocket/src/main/java/com/crocket/Hoop.java
+++ crocket/src/main/java/com/crocket/Hoop.java
@@ -1,12 +1,15 @@
 package crocket.src.main.java.com.crocket;
 public class Hoop extends Entity {
 
-
+    private boolean reached;
 
     public Hoop(int width, int height, double xPosition, double yPosition, Direction dir) {
         super(width, height, xPosition, yPosition);
         //TODO Auto-generated constructor stub
     }
-
+    
+    public void reset(){
+        reached = false;
+    }
 
 }
--- crocket/src/main/java/com/crocket/Peg.java
+++ crocket/src/main/java/com/crocket/Peg.java
@@ -1,11 +1,16 @@
 package crocket.src.main.java.com.crocket;
-import crocket.src.main.java.com.crocket.Entity;
 
 public class Peg extends Entity {
+
+    private boolean reached;
 
     public Peg(int width, int height, double xPosition, double yPosition) {
         super(width, height, xPosition, yPosition);
         //TODO Auto-generated constructor stub
+    }
+
+    public void reset(){
+        reached = false;
     }
     
 }
