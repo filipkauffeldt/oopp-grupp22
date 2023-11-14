72b39f224d1df1ad578b985ce42632efb84ab312
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2023-11-14 22:26:34 +0100

    Use collidable and movable interfaces

--- crocket/src/main/java/com/crocket/MovableObstacle.java
+++ crocket/src/main/java/com/crocket/MovableObstacle.java
@@ -1,6 +1,6 @@
 package com.crocket;
 
-public class MovableObstacle extends Obstacle implements IMovable{
+public abstract class MovableObstacle extends Obstacle implements IMovable{
     private double xVelocity;
     private double yVelocity;
 
@@ -11,5 +11,21 @@ public class MovableObstacle extends Obstacle implements IMovable{
     public void move(){
         this.setxPosition((this.getxPosition()+xVelocity));
         this.setyPosition((this.getyPosition()+yVelocity));
+    }
+
+    public double getxVelocity() {
+        return xVelocity;
+    }
+
+    public double getyVelocity() {
+        return yVelocity;
+    }
+
+    public void setxVelocity(double xVelocity) {
+        this.xVelocity = xVelocity;
+    }
+
+    public void setyVelocity(double yVelocity) {
+        this.yVelocity = yVelocity;
     }
 }
--- crocket/src/main/java/com/crocket/Obstacle.java
+++ crocket/src/main/java/com/crocket/Obstacle.java
@@ -1,6 +1,6 @@
 package com.crocket;
 
-public abstract class Obstacle extends Entity {
+public abstract class Obstacle extends Entity implements ICollidable{
 
     public Obstacle(int width, int height, double xPosition, double yPosition) {
         super(width, height, xPosition, yPosition);
