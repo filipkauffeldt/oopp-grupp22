99571f276677e540c123ef31058e3af86d0d2abd
adiwir <adrian@avadi.se>
2023-11-14 10:38:54 +0100

    Added more textures

--- crocket/src/main/java/com/crocket/Draw.java
+++ crocket/src/main/java/com/crocket/Draw.java
@@ -5,23 +5,36 @@ import java.awt.*;
 
 public class Draw extends Canvas {
     
+    //temp placement
     Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
     Image ballImg = Toolkit.getDefaultToolkit().getImage("crocket/assets/textures/Ball.png");
     Image grassImg = Toolkit.getDefaultToolkit().getImage("crocket/assets/textures/Weed.png");
+    Image sandImg = Toolkit.getDefaultToolkit().getImage("crocket/assets/textures/Anakin.png");
+    Image iceImg = Toolkit.getDefaultToolkit().getImage("crocket/assets/textures/Nice.png");
 
+    //temp positioning
     public void paint(Graphics g){
         
-        for(int x = 0; x < screen.width; x+=100){
-            for(int y = 0; y < screen.height; y+=100){        
+        for(int x = 0; x < screen.width/2; x+=100){
+            for(int y = 0; y < screen.height/2; y+=100){        
+                g.drawImage(iceImg, x, y, this);
+            }
+            for(int y = screen.height/2; y < screen.height; y+=100){
                 g.drawImage(grassImg, x, y, this);
             }
         }
+
+        for(int x = screen.width/2; x < screen.width; x+=100){
+            for(int y = 0; y < screen.height/2; y+=100){        
+                g.drawImage(iceImg, x, y, this);
+            }
+            for(int y = screen.height/2; y < screen.height; y+=100){
+                g.drawImage(sandImg, x, y, this);
+            }
+        }
         
-        g.drawImage(ballImg, 140*(screen.width/600), 100*(screen.height/400), this);
+        g.drawImage(ballImg, 1000, 600, this);
     }
 
-    public void render(Graphics g){
-        
-        g.translate(540, 160);
-    }
+    
 }
