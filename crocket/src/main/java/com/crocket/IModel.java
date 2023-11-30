7dd7f7b968df936cfcc7d2d2c7e745879ebece63
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2023-11-30 10:00:43 +0100

    Update Model interface

--- crocket/src/main/java/com/crocket/IModel.java
+++ crocket/src/main/java/com/crocket/IModel.java
@@ -1,9 +1,12 @@
 package com.crocket;
 
+import java.util.List;
 import java.util.Set;
 
 /**
  * Facade for the model of the game.
+ * Requires an ILevel to be set with setLevel().
+ * Requires at least one Player to be added.
  */
 public interface IModel {
 
@@ -14,7 +17,7 @@ public interface IModel {
     public Set<DrawableEntity> getDrawableEntities();
 
     /**
-     * Returns the tilemap representaion of the surfaces in the current level.
+     * Returns the tilemap representation of the surfaces in the current level.
      * @return Surface[][] levelTilemap
      */
     public Surface[][] getLevelTilemap();
@@ -46,4 +49,31 @@ public interface IModel {
      * @param degrees
      */
     public void updateAim(int degrees);
+
+    /**
+     * Decrements the aim angle of the active player's ball.
+     */
+    public void aimRight();
+
+    /**
+     * Increments the aim angle of the active player's ball.
+     */
+    public void aimLeft();
+
+    /**
+     * Overwrites the model's List of Player with a new List of Player .
+     * @param players
+     */
+    public void setPlayers(List<Player> players);
+
+    /**
+     * Adds a Player to the model's List of Player.
+     * @param player
+     */
+    public void addPlayer(Player player);
+
+    /**
+     * Removes all players from the model.
+     */
+    public void clearPlayers();
 }
