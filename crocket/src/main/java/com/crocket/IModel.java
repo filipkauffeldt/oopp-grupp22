78559c60a7f48b7d7be971f28b2a994a8c033843
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2023-12-01 13:24:21 +0100

    Update IModel

--- crocket/src/main/java/com/crocket/IModel.java
+++ crocket/src/main/java/com/crocket/IModel.java
@@ -12,18 +12,22 @@ public interface IModel {
 
     /**
      * Returns a drawable representation of the entities in the current level.
+     * 
      * @return Set<DrawableEntity> drawableEntities
      */
     public Set<DrawableEntity> getDrawableEntities();
 
     /**
      * Returns the tilemap representation of the surfaces in the current level.
+     * 
      * @return Surface[][] levelTilemap
      */
-    public Surface[][] getLevelTilemap();
+    public Surface[][] getLevelSurfacemap();
 
     /**
-     * Sets the current level and populates the entities set with the entities from the level.
+     * Sets the current level and populates the entities set with the entities from
+     * the level.
+     * 
      * @param level
      */
     public void setLevel(ILevel level);
@@ -33,41 +37,46 @@ public interface IModel {
      */
     public void restartLevel();
 
-   /**
-    * Updates the model. Moves any movable entities and checks for collisions.
-    */
+    /**
+     * Updates the model. Moves any movable entities and checks for collisions.
+     */
     public void update();
 
     /**
-     * Shoots the ball of the active with the given power in the direction specified by the direction set in update aim.
+     * Shoots the ball of the active with the given power in the direction specified
+     * by the direction set in update aim.
+     * 
      * @param power
      */
     public void shootBall(int power);
 
     /**
      * Updates the aim of the active player's ball.
+     * 
      * @param degrees
      */
     public void updateAim(int degrees);
 
     /**
-     * Decrements the aim angle of the active player's ball.
+     * Increments the aim angle of the active player's ball.
      */
     public void aimRight();
 
     /**
-     * Increments the aim angle of the active player's ball.
+     * Decrements the aim angle of the active player's ball.
      */
     public void aimLeft();
 
     /**
      * Overwrites the model's List of Player with a new List of Player .
+     * 
      * @param players
      */
     public void setPlayers(List<Player> players);
 
     /**
      * Adds a Player to the model's List of Player.
+     * 
      * @param player
      */
     public void addPlayer(Player player);
@@ -76,4 +85,10 @@ public interface IModel {
      * Removes all players from the model.
      */
     public void clearPlayers();
+
+    /**
+     * 
+     * @return true if the model is ready to accept a shot, false otherwise.
+     */
+    public boolean shotAllowed();
 }
