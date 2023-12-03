e82731eff07e9e8b08b56282bd524164bb9d8e02
Oskmey <qcube.gaming@gmail.com>
2023-12-03 13:27:29 +0100

    Update Texturemanger

--- crocket/src/main/java/com/crocket/TextureManager.java
+++ crocket/src/main/java/com/crocket/TextureManager.java
@@ -1,23 +1,23 @@
 package com.crocket;
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+
+
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.Map;
 import javax.imageio.ImageIO;
+import java.util.List;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 
 public class TextureManager {
 
     private static final String JSON_FILE_PATH = "crocket\\assets\\textures\\Textures.json";
-    private static final String SURFACE_TEXTURE_LIST = "SurfaceTextureList";
-    private static final String ENTITY_TEXTURE_LIST = "EntityTextureList";
+
 
 
     private Map<String, BufferedImage> textureCacheMap;
@@ -26,45 +26,26 @@ public class TextureManager {
         this.textureCacheMap = new HashMap<>();
     }
 
+    protected void loadTextures() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            List<Map<String, String>> textureMap = mapper.readValue(new File(JSON_FILE_PATH), new TypeReference<List<Map<String, String>>>() {});
+            for (Map<String, String> texture : textureMap) {
+                String textureName = texture.get("Name");
+                String texturePath = texture.get("TexturePath");
+                BufferedImage textureImage = ImageIO.read(new File(texturePath));
+                textureCacheMap.put(textureName, textureImage);
+            }
+        } catch (Exception ex) {
+            System.out.println("Error loading textures: " + ex.getMessage());
+        }
+    }
+
+
     public BufferedImage getTexture(String textureName) {
         if (!textureCacheMap.containsKey(textureName)) {
             throw new IllegalArgumentException("Invalid texture name " + textureName);
         }
         return textureCacheMap.get(textureName);
-    }
-
-    protected synchronized void loadTextures() {
-        JSONParser parser = new JSONParser();
-        try {
-            FileReader reader = new FileReader(JSON_FILE_PATH);
-            JSONArray textureTypes = (JSONArray) parser.parse(reader);
-            for (Object type : textureTypes) {
-                JSONObject TextureObject = (JSONObject) type;
-                JSONArray TextureObjectList = (JSONArray) TextureObject.get(SURFACE_TEXTURE_LIST);
-                if (TextureObjectList != null) {
-                    for (Object object : TextureObjectList) {
-                        convertDataToImage(object);
-                    }
-                }
-                TextureObjectList = (JSONArray) TextureObject.get(ENTITY_TEXTURE_LIST);
-                if (TextureObjectList != null) {
-                    for (Object object : TextureObjectList) {
-                        convertDataToImage(object);
-                    }
-                }
-            }
-        } catch (ParseException ex) {
-            System.out.println("Error parsing textures.json file");
-        } catch (IOException ex) {
-            System.out.println("Error bad path to texture in json file");
-        }
-    }
-
-    private void convertDataToImage(Object object) throws IOException {
-        JSONObject textureObjectJSON = (JSONObject) object;
-        String textureName = (String) textureObjectJSON.get("Name");
-        String texturePath = (String) textureObjectJSON.get("TexturePath");
-        BufferedImage texture = ImageIO.read(new File(texturePath));
-        textureCacheMap.put(textureName, texture);
     }
 }
