596729ef6f9cbcf3cf3fd567dab706be6e77fa5c
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2023-12-11 23:36:48 +0100

    Remove update logic from controller

--- crocket/src/main/java/com/crocket/controller/CroquetController.java
+++ crocket/src/main/java/com/crocket/controller/CroquetController.java
@@ -2,26 +2,20 @@ package com.crocket.controller;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Set;
-
-import com.crocket.model.DrawableEntity;
 import com.crocket.model.interfaces.IModel;
 import com.crocket.view.CroquetView;
-import com.crocket.view.interfaces.ILevelView;
 
 public class CroquetController implements KeyListener {
 
     private CroquetView view;
-    private ILevelView levelView;
     private IModel model;
 
     private double power;
     private int maxPower = 40;
     private boolean powerIsMax = false;
 
-    public CroquetController(CroquetView view, ILevelView levelView, IModel model) {
+    public CroquetController(CroquetView view, IModel model) {
         this.view = view;
-        this.levelView = levelView;
         this.model = model;
 
         view.addKeyListener(this);
@@ -72,13 +66,5 @@ public class CroquetController implements KeyListener {
 
     public void keyTyped(KeyEvent ke) {
 
-    }
-
-    public void update() {
-        model.update();
-
-        Set<DrawableEntity> drawableEntities = model.getDrawableEntities();
-
-        levelView.drawEntities(drawableEntities);
     }
 }
