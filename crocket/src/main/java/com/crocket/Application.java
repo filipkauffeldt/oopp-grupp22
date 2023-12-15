4ca3bbdc0e5bd8c0420324667e5f093cac8d7f50
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2023-12-15 12:07:33 +0100

    Feature, added possibility to play a test level

--- crocket/src/main/java/com/crocket/Application.java
+++ crocket/src/main/java/com/crocket/Application.java
@@ -6,6 +6,7 @@ import com.crocket.controller.CroquetController;
 import com.crocket.model.EventPublisher;
 import com.crocket.model.Level;
 import com.crocket.model.Level1;
+import com.crocket.model.LevelTest;
 import com.crocket.model.Model;
 import com.crocket.model.Player;
 import com.crocket.model.entity.Ball;
@@ -23,8 +24,11 @@ public class Application {
 
     private static void init() {
         CroquetView frame = CroquetView.getInstance();
-        Level level1 = new Level1();
-        LevelView level1View = new LevelView();
+        // Level level1 = new Level1();
+        // LevelView level1View = new LevelView();
+        Level levelTest = new LevelTest();
+        LevelView levelTestView = new LevelView();
+
 
         IModel model = Model.getInstance();
         List<Player> players = new ArrayList<Player>();
@@ -32,8 +36,10 @@ public class Application {
         EventPublisher eventPublisher = EventPublisher.getInstance();
         eventPublisher.addListener(Model.getInstance());
 
-        Ball p1Ball = new Ball(19, 19, 1030d, 650d, 20);
-        Ball p2Ball = new Ball(19, 19, 1070d, 650d, 20);
+        // Ball p1Ball = new Ball(19, 19, 1030d, 650d, 20); // For level 1
+        // Ball p2Ball = new Ball(19, 19, 1070d, 650d, 20); // For level 1
+        Ball p1Ball = new Ball(19, 19, 400d, 450d, 20); // For level test
+        Ball p2Ball = new Ball(19, 19, 430d, 480d, 20); // For level test
 
         // TODO: Implement a way for the end user to decide how many players there should be
         Player player1 = new Player(p1Ball, "Player 1");
@@ -41,12 +47,15 @@ public class Application {
         players.add(player1);
         players.add(player2);
 
-        model.setLevel(level1);
+        // model.setLevel(level1);
+        model.setLevel(levelTest);
         model.setPlayers(players);
 
         CroquetController controller = new CroquetController(frame, model);
-        level1View.setSurfaceMap(level1.getLevelSurfacemap(), level1.getLevelHeight(), level1.getLevelWidth());
-        frame.setLevelView(level1View);
+        // level1View.setSurfaceMap(level1.getLevelSurfacemap(), level1.getLevelHeight(), level1.getLevelWidth());
+        // frame.setLevelView(level1View);
+        levelTestView.setSurfaceMap(levelTest.getLevelSurfacemap(), levelTest.getLevelHeight(), levelTest.getLevelWidth());
+        frame.setLevelView(levelTestView);
         Thread run = new Thread();
         run.start();
 
